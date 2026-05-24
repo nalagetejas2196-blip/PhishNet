0d5b46cedda8879f2f34def5cd1eabd6528fc60a
Tejas Nalage <nalagetejas2196@gmail.com>
2026-05-24 16:26:13 +0530

    Fix port for Railway

--- app.py
+++ app.py
@@ -68,4 +68,6 @@ def check():
     return jsonify({'result': 'SAFE' if prediction == 0 else 'PHISHING'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
